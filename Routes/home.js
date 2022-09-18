8266049854cbefba020651e72a07e90735288c46
ziad0shmnady <zeiadmohamed331@gmail.com>
2022-09-18 05:54:03 +0200

    product details

--- Routes/home.js
+++ Routes/home.js
@@ -4,129 +4,28 @@ let User = require('../model/user')
 let Product = require('../model/product')
 let bcrypt = require('bcryptjs')
 let passport = require('passport')
+const productController = require('../controller/productController')
+const userController = require('../controller/userController')
 let { ensureAuthenticated } = require('../config/auth')
 
-router.get("/", (req, res) => {
-    res.redirect("/home")
-})
+router.get("/", userController.getHome)
 
-router.get("/home", async (req, res) => {
+router.get("/home", productController.getAllProduct);
 
-    let products;
-    try {
-        products = await Product.find();
-    } catch (err) {
-        console.log(err)
-    }
-    if (!products) {
-        return res.status(404).json({ message: "No products found !" })
-    }
-    return res.status(201).render('HomePage',
-        {
-            title: 'Products',
-            user: req.user,
-            arr: products
-        })
-});
-
-router.get("/home/login", (req, res) => {
-    res.render("Login", { user: req.user, title: "Login" });
-});
+router.get("/home/login", userController.getLogin);
 
 // Login handler 
-router.post('/home/login', (req, res, next) => {
-    passport.authenticate('local', {
-        successRedirect: '/home',
-        failureRedirect: '/home/login',
-        failureFlash: true
-    })(req, res, next)
-})
+router.post('/home/login', userController.postlogin)
 
-router.get("/home/register", (req, res) => {
-    res.render("Register", { user: req.user, title: "Register" });
-});
+router.get("/home/register", userController.getRegister);
 
-router.post("/home/register", (req, res) => {
-    let { firstname, lastname, email, password, password2 } = req.body;
-    let errors = []
+router.post("/home/register", userController.postRegister);
 
-    //check that all fields are filled
-    if (!firstname || !lastname || !email || !password || !password2) {
-        errors.push({ msg: "Please fill out all the fields" })
-    }
+router.get("/home/profile", ensureAuthenticated, userController.getProfile);
 
-    //check that both passwords matches
-    if (password !== password2) {
-        errors.push({ msg: "Passwords don't match" })
-    }
+router.delete('/user/:id', userController.deleteUser)
 
-    //check that password is at least 6 characters long
-    if (password.length < 6) {
-        errors.push({ msg: 'Passowrd should be at least 6 characters' })
-    }
-
-    //check if their is any errors 
-    if (errors.length > 0) {
-        res.render('register', { errors, firstname, lastname, email, password, password2, title: "Register" })
-    } else {
-        User.findOne({ email: email })
-            .then(user => {
-                errors.push({ msg: "Email is already registered" })
-                if (user) {
-                    res.render('register', {
-                        user: req.user,
-                        errors,
-                        firstname,
-                        lastname,
-                        email,
-                        password,
-                        password2,
-                        title: "Register"
-                    })
-                } else {
-                    // if user doesn't exist create new one
-                    let newUser = new User({ firstname, lastname, email, password })
-                    //crypt password
-                    bcrypt.genSalt(10, (err, salt) => {
-                        bcrypt.hash(newUser.password, salt, (err, hash) => {
-                            if (err) throw err
-                            //password is hashed 
-                            newUser.password = hash
-                            //SaveUser
-                            newUser.save()
-                                .then(user => {
-                                    req.flash('success_msg', 'You are now registered and can Login ')
-                                    res.redirect('/home/login')
-                                })
-                                .catch(err => console.log(err))
-                        })
-                    })
-                }
-            })
-    }
-});
-
-router.get("/home/profile", ensureAuthenticated, (req, res) => {
-    res.render("Profile", { user: req.user, title: "Home" });
-});
-
-router.delete('/user/:id', (req, res) => {
-    let id = req.params.id;
-    User.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ redirect: '/home' })
-        })
-        .catch(err => {
-            console.log(err)
-        })
-})
-
-router.get('/home/logout', (req, res, next) => {
-    req.logout((err) => {
-        if (err) return next(err)
-        res.redirect('/home');
-    });
-});
+router.get('/home/logout', userController.logoutUser);
 
 router.get('/productDetails', (req, res) => {
     res.render('productDetails', {
@@ -134,80 +33,10 @@ router.get('/productDetails', (req, res) => {
         user: req.user
     })
 })
-// router.get('/home', async (req, res, next) => {
-//     let products;
-//     try {
-//         products = await Product.find();
-//     } catch (err) {
-//         console.log(err)
-//     }
-//     if (!products) {
-//         return res.status(404).json({ message: "No products found !" })
-//     }
-//     return res.status(201).render('HomePage',
-//         {
-//             title: 'Products',
-//             user: req.user,
-//             arr: products
-//         })
-// })
 
-router.post('/addProduct', async (req, res, next) => {
-    const name = req.body.name;
-    const category = req.body.category
-    const image = req.body.image;
-    const price = req.body.price;
-    try {
-        let product = new Product({
-            name: name,
-            category: category,
-            price: price,
-            image: image
-        })
-        await product.save()
-    } catch (err) {
-        console.log(err)
-    }
-    if (!product) {
-        return res.status(500).json({ message: 'unable To add' })
-    } return res.status(201).json({ product })
-})
+router.post('/addProduct', productController.addProduct)
 
-router.get('/productDetails/:id', async (req, res, next) => {
-    const id = req.params.id
-    let product;
-    try {
-        product = await product.findOne({ _id: req.params.id })
-    }
+router.get('/productDetails/:id', productController.getDetails)
 
-    catch (err) {
-        console.log(err)
-    }
-    if (!product) {
-        return res.status(500).json({ message: 'Not product found' })
-    } return res.status(201).json({ product })
-})
-
-router.get('/home/laptops', async (req, res) => {
-
-    let products;
-    try {
-
-        products = await Product.find({ "category": "Laptop" })
-
-    } catch (err) {
-        console.log(err)
-    }
-    if (!products) {
-        return res.status(404).json({ message: "No products found !" })
-    }
-    return res.status(201).render('laptops',
-        {
-            title: 'Products',
-            user: req.user,
-            arr: products
-        })
-
-
-})
+router.get('/home/laptops', productController.getLaptops)
 module.exports = router
---
+++ controller/productController.js
@@ -0,0 +1,81 @@
+let express = require('express')
+let router = express.Router()
+let Product = require('../model/product')
+
+exports.getAllProduct = async (req, res) => {
+
+  let products;
+  try {
+    products = await Product.find();
+  } catch (err) {
+    console.log(err)
+  }
+  if (!products) {
+    return res.status(404).json({ message: "No products found !" })
+  }
+  return res.status(201).render('HomePage',
+    {
+      title: 'Products',
+      user: req.user,
+      arr: products
+    })
+}
+exports.addProduct = async (req, res, next) => {
+  const name = req.body.name;
+  const category = req.body.category
+  const image = req.body.image;
+  const price = req.body.price;
+  let product
+  try {
+    product = new Product({
+      name: name,
+      category: category,
+      price: price,
+      image: image
+    })
+    await product.save()
+  } catch (err) {
+    console.log(err)
+  }
+  if (!product) {
+    return res.status(500).json({ message: 'unable To add' })
+  } return res.status(201).json({ product })
+}
+exports.getDetails = async (req, res, next) => {
+  const id = req.params.id
+  let product;
+  try {
+    product = await Product.findOne({ _id: req.params.id })
+  }
+  catch (err) {
+    console.log(err)
+  }
+  if (!product) {
+    return res.status(500).json({ message: 'Not product found' })
+  } return res.status(201).render('details', {
+    product: product,
+    title: 'Product Details',
+    user: req.user,
+  })
+}
+exports.getLaptops = async (req, res) => {
+
+  let products;
+  try {
+
+    products = await Product.find({ "category": "Laptop" })
+
+  } catch (err) {
+    console.log(err)
+  }
+  if (!products) {
+    return res.status(404).json({ message: "No products found !" })
+  }
+  return res.status(201).render('laptops',
+    {
+      title: 'Products',
+      user: req.user,
+      arr: products
+    })
+
+}
